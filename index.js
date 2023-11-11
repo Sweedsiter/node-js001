ef2950290310d1188cf29816271be4e8ec4ee8f3
CodeSandbox <support@codesandbox.io>
2023-11-11 02:10:01 +0000

    Sandbox snapshot commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello peter");
+const express = require("express");
+const app = express();
+const port = 8002;
+
+app.get("/", (req, res) => {
+  res.send("This is Home Page");
+});
+
+app.listen(port, "localhost", () => {
+  console.log("start port 8080");
+});
+module.exports = app;
