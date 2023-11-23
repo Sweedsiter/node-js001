f9a4e2fd15596c5b9c286bee1220fbe829fe6d00
CodeSandbox <support@codesandbox.io>
2023-11-23 08:05:18 +0000

    Sandbox snapshot commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const router = require("./routs/router");
 const app = express();
 const port = 8002;
 
 app.get("/", (req, res) => {
   res.send("This is Home Page");
 });
+// routers
+app.use("/product", router);
 
 app.listen(port, "localhost", () => {
-  console.log("start port 8080");
+  console.log("start port 8002");
 });
 module.exports = app;
